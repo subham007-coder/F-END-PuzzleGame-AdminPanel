c5bd57cc66468055fade73051e5842522e55f979
subham007-coder <dsubham109@gmail.com>
2025-03-21 22:11:03 +0530

    Add production configuration and update project structure for deployment

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-const config = {
-    API_URL: import.meta.env.PROD
-        ? 'https://full-stack-music-backend.onrender.com/api'
-        : 'http://localhost:5000/api'
-};
+// const config = {
+//     API_URL: import.meta.env.PROD
+//         ? 'https://full-stack-music-backend.onrender.com/api'
+//         : 'http://localhost:5000/api'
+// };
 
-export default config;
+
+// const config = {
+//     API_URL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+//   };
+// export default config;
